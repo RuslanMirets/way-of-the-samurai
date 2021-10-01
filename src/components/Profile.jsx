cc9e7edb804da2cf031c820368f8b88328c68dda
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-01 12:58:35 +0300

    Commit Before lesson #12

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="main-banner">
+        <img src="https://w-dog.ru/wallpapers/13/14/465473241668895/derevo-listya-priroda-vetvi.jpg" alt="main"/>
+      </div>
+      <div>
+        ava + desc
+      </div>
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <nav>
+        <ul>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
