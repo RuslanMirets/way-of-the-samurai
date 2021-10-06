54fcb995363512014e9b9a0517e647359db2756c
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-06 17:32:05 +0300

    Урок №16

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,10 @@
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={classes.item}>
       <img src="https://im0-tub-ru.yandex.net/i?id=5275620b9c69d4c19886fcea87348990&n=13&exp=1" alt="avatar"/>
-      post 1
+      {props.message}
     </div>
   );
 }
