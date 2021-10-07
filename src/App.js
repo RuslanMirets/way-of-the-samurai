82bb88b6edbbc4f5f74309e951c526bd242c3d74
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-07 11:17:09 +0300

    Урок №22

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
         <Sidebar/>
         <div className="app-wrapper-content">
           <Route path='/profile' component={Profile}/>
-          <Route path='/dialogs' component={Dialogs}/>
+          <Route exact path='/dialogs' component={Dialogs}/>
           <Route path='/news' component={News}/>
           <Route path='/music' component={Music}/>
           <Route path='/settings' component={Settings}/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,29 @@
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={classes.dialog + ' ' + classes.active}>
-          Ivan
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/1" activeClassName={classes.active}>
+            Ivan
+          </NavLink>
         </div>
         <div className={classes.dialog}>
-          Petr
+          <NavLink to="/dialogs/2" activeClassName={classes.active}>
+            Petr
+          </NavLink>
         </div>
         <div className={classes.dialog}>
-          Sveta
+          <NavLink to="/dialogs/3" activeClassName={classes.active}>
+            Sveta
+          </NavLink>
         </div>
         <div className={classes.dialog}>
-          Sasha
+          <NavLink to="/dialogs/4" activeClassName={classes.active}>
+            Sasha
+          </NavLink>
         </div>
       </div>
       <div className={classes.messages}>
