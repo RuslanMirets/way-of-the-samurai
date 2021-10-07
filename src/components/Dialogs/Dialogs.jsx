432303022f6cc4371530f095aa392d1888028f2f
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-07 11:02:24 +0300

    Урок №21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,26 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog + ' ' + classes.active}>
+          Ivan
+        </div>
+        <div className={classes.dialog}>
+          Petr
+        </div>
+        <div className={classes.dialog}>
+          Sveta
+        </div>
+        <div className={classes.dialog}>
+          Sasha
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>How are you?</div>
+        <div className={classes.message}>Ha ha</div>
+      </div>
     </div>
   );
 }
