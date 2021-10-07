a13f66d198902d9b705cdefdefabd67e083d8dcf
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-07 09:54:44 +0300

    Урок №18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className={classes.profile}>
+    <div>
       <div className={classes.profileBanner}>
         <img src="https://w-dog.ru/wallpapers/13/14/465473241668895/derevo-listya-priroda-vetvi.jpg" alt="main"/>
       </div>
