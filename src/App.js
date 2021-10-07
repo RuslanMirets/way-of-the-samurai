f79797a7c8fa199a195f440de7c6a24e8e0da218
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-07 10:24:28 +0300

    Урок №19

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <Sidebar/>
-      <div className="app-wrapper-content">
-        {/*<Profile/>*/}
-        <Dialogs/>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header/>
+        <Sidebar/>
+        <div className="app-wrapper-content">
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import classes from './Header.module.css';
 const Header = () => {
   return (
     <header className={classes.header}>
-      <img src={logo} alt="logo"/>
+      <a href="/">
+        <img src={logo} alt="logo"/>
+      </a>
     </header>
   );
 }
