77f850e6d76fa286b0f59b29a6a80276311c241d
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-08 12:18:54 +0300

    Урок №35

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import App from './App';
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
