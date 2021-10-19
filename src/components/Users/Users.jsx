4908e8bf58284d80e5fcce26cdcf45006494ef50
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-19 17:18:06 +0300

    Урок №62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import classes from "./Users.module.css";
 import userPhoto from "../../assets/images/avatar.jpg";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -33,9 +34,23 @@ let Users = (props) => {
             </div>
             <div>
               {user.followed ? <button onClick={() => {
-                props.unfollow(user.id)
+                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+                  withCredentials: true,
+                  headers: {"API-KEY": "d4daa6c0-6ee3-404b-811e-1388efb47a2e"}
+                }).then(response => {
+                  if (response.data.resultCode === 0) {
+                    props.unfollow(user.id)
+                  }
+                });
               }}>Unfollow</button> : <button onClick={() => {
-                props.follow(user.id)
+                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {}, {
+                  withCredentials: true,
+                  headers: {"API-KEY": "d4daa6c0-6ee3-404b-811e-1388efb47a2e"}
+                }).then(response => {
+                  if (response.data.resultCode === 0) {
+                    props.follow(user.id)
+                  }
+                });
               }}>Follow</button>}
             </div>
           </span>
