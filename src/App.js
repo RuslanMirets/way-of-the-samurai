644d24bae798df684f8165d3876b97211aa0d027
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-19 14:37:03 +0300

    Урок №61

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
@@ -8,11 +7,12 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header/>
+      <HeaderContainer/>
       <Sidebar/>
       <div className="app-wrapper-content">
         <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,15 @@ import logo from '../../logo.svg';
 import classes from './Header.module.css';
 import {Link} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
       <Link to="/">
         <img src={logo} alt="logo"/>
       </Link>
+      <div className={classes.loginBlock}>
+        {props.isAuth ? props.login : <Link to={'/login'}>Login</Link>}
+      </div>
     </header>
   );
 }
