2a80ea2a9bb91c31508e97a43382bca3537f5582
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-10-21 09:42:29 +0300

    Урок №63

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {"API-KEY": "d4daa6c0-6ee3-404b-811e-1388efb47a2e"}
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 5) {
+    return (
+      instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    )
+  }
+}
